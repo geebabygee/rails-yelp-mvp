40e7c397a2d90eafdbf147c38fdd44ef79f5ec6c
Irina Popa <irina.oana.popa@gmail.com>
2018-11-12 09:28:54 +0000

    render 'form' restaurant: @restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+    root to: 'restaurants#index'
+
     resources :restaurants, only: [ :index, :new, :show, :create] do
       resources :reviews, only: [ :new, :create]
    end
